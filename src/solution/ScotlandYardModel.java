dcf1a4693b1de74689b5904db48b62b11a21e124
Michael Aldridge <mikea3113@gmail.com>
2015-03-09 11:05:54 +0000

    Dan modified ScotlandYard.java to commence game setup (part 1)
    - private filds added for storing game data
    - constructor added
    - join() added
    - getPlayers() added
    - isReady() added
    - sortColours() added
    
    some issued still to be resolved with the above functions - ses comments in code

--- src/solution/ScotlandYardModel.java
+++ src/solution/ScotlandYardModel.java
@@ -6,13 +6,42 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.*;
 
 public class ScotlandYardModel extends ScotlandYard {
 
+private final Graph londonGraph;
+private Colour currentPlayer;
+private final Map<Colour,Player> colourToPlayer;
+private Map<Colour,Integer> colourToLocation;
+private Map<Colour,Map<Ticket,Integer>> colourToTickets;
+private List<Colour> orderOfPlay; 
+private int numberOfDetectives;
+private final List<Boolean> showRounds; 
 
-    public ScotlandYardModel(int numberOfDetectives, List<Boolean> rounds, String graphFileName) throws IOException {
-        super(numberOfDetectives, rounds, graphFileName);
+    //Class constructor reads and stores graph and game attributes (e.g. num of players, show rounds). 	
+    public ScotlandYardModel(int numberOfDetectives, List<Boolean> rounds, String graphFileName) { 
+        super(numberOfDetectives, rounds, graphFileName); // ask TA
+        
+		ScotlandYardGraphReader reader = new ScotlandYardGraphReader();
+		Graph<Integer,Route> graph = null;
+		try {
+			graph = reader.readGraph(graphFileName);
+			
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+			
+		}
+		londonGraph = graph;
+		this.numberOfDetectives = numberOfDetectives;
+		showRounds = rounds;
+		
+		List<Colour> orderOfPlay = new ArrayList<Colour>();		
+		colourToPlayer = new HashMap<Colour,Player>();
+		colourToTickets = new HashMap<Colour,Map<Ticket,Integer>>();
+		colourToLocation = new HashMap<Colour, Integer>();
     }
+
 
     @Override
     protected Move getPlayerMove(Colour colour) {
@@ -49,14 +78,23 @@ public class ScotlandYardModel extends ScotlandYard {
 
     }
 
-    @Override
+    //Adds a player to the game, storing the player's colour, location and tickets. 
+    @Override 
     public boolean join(Player player, Colour colour, int location, Map<Ticket, Integer> tickets) {
-        return false;
+	if(orderOfPlay.contains(colour)) return false; //how should we handle false return? 
+    	else {
+	    	orderOfPlay.add(colour);
+	    	sortColours(orderOfPlay);
+	    	colourToPlayer.put(colour,player);
+	    	colourToLocation.put(colour,location);
+	    	colourToTickets.put(colour,tickets);
+   	}
+        return true;
     }
 
     @Override
     public List<Colour> getPlayers() {
-        return null;
+        return orderOfPlay; // do we want a separate list for players not in order of play?
     }
 
     @Override
@@ -66,12 +104,13 @@ public class ScotlandYardModel extends ScotlandYard {
 
     @Override
     public int getPlayerLocation(Colour colour) {
-        return 0;
+    	return colourToLocation.get(colour);
     }
 
     @Override
     public int getPlayerTickets(Colour colour, Ticket ticket) {
-        return 0;
+        Map<Ticket,Integer> ticketMap = colourToTickets.get(colour);
+        return ticketMap.get(ticket);
     }
 
     @Override
@@ -79,9 +118,12 @@ public class ScotlandYardModel extends ScotlandYard {
         return false;
     }
 
+    //Checks whether enough players have been added and that black is to start. 
     @Override
     public boolean isReady() {
-        return false;
+    	if(orderOfPlay.size() == numberOfDetectives +1 && orderOfPlay.get(0).equals(Colour.Black)) {
+    		return true;
+    	} else return false;
     }
 
     @Override
@@ -97,5 +139,18 @@ public class ScotlandYardModel extends ScotlandYard {
     @Override
     public List<Boolean> getRounds() {
         return null;
+    }
+
+    //Ensures Black is at the front of the list. 
+    private List<Colour> sortColours(List<Colour> colours) {
+	int index = colours.indexOf(Colour.Black); //not sure this the best way to implement? 
+	if(index != -1) {
+		colours.remove(index);
+		Colour firstColour = colours.get(0);
+		colours.remove(0);
+		colours.add(0,Colour.Black);
+		colours.add(index,firstColour);	
+	}
+	return colours;
     }
 }
