90b20d343a64cd3987db95a842ef646ce80b8b50
Michael Aldridge <mikea3113@gmail.com>
2015-03-10 19:34:36 +0000

    partly done

--- src/solution/ScotlandYardModel.java
+++ src/solution/ScotlandYardModel.java
@@ -40,12 +40,20 @@ private final List<Boolean> showRounds;
 		colourToPlayer = new HashMap<Colour,Player>();
 		colourToTickets = new HashMap<Colour,Map<Ticket,Integer>>();
 		colourToLocation = new HashMap<Colour, Integer>();
+		
+		currentPlayer = Colour.valueOf("Black");
     }
 
 
     @Override
-    protected Move getPlayerMove(Colour colour) {
-        return null;
+    protected Move getPlayerMove(Colour colour) 
+    {
+       int location = colourToLocation.get(colour);
+       Player player = colourToPlayer.get(colour);
+       List<Move> validMoves = validMoves(colour);
+       Move playerMove = player.notify(location, validMoves);
+       if (validMoves.contains(playerMove)) return playerMove;
+       else return playerMove; // what should we do if player retuens bad move ????
     }
 
     @Override
@@ -69,8 +77,171 @@ private final List<Boolean> showRounds;
     }
 
     @Override
-    protected List<Move> validMoves(Colour player) {
-        return null;
+    protected List<Move> validMoves(Colour player) 
+    {
+        int location = colourToLocation.get(player);
+        List<Move> makableMoves = new ArrayList();
+        List<MoveTicket> singleMoves = singleMoves(location, player);
+        List<MoveDouble> doubleMoves = doubleMoves(player);
+        for(MoveTicket move : singleMoves)
+        {
+        	if(player.equals(Colour.valueOf("Black")))
+        	{
+        		int numSecret = getPlayerTickets(Colour.valueOf("Black"), Ticket.valueOf("SecretMove"));
+        		if( enoughTicketsMrX(move, numSecret) && !moveOcupyTest(move) )
+        		{
+        			makableMoves.add(move);
+        		}
+        	}
+        	else
+        	{
+        		int numBus = getPlayerTickets(player, Ticket.valueOf("Bus"));
+				int numTaxi = getPlayerTickets(player, Ticket.valueOf("Taxi"));
+				int numUnderground = getPlayerTickets(player, Ticket.valueOf("Underground"));
+        		if( enoghTicketsDetective(move, numBus, numTaxi, numUnderground) && !moveOcupyTest(move) )
+        		{
+        			makableMoves.add(move);
+        		}
+        	}
+        }
+        if(player.equals(Colour.valueOf("Black")))
+        {
+		    for(MoveDouble move : doubleMoves)
+		    {
+		    		int numSecret = getPlayerTickets(Colour.valueOf("Black"), Ticket.valueOf("SecretMove"));
+		    		if( enoughTicketsMrX(move, numSecret) && !moveOcupyTest(move) )
+		    		{
+		    			makableMoves.add(move);
+		    		}
+		    }
+        }
+        return makableMoves;
+    }
+    
+    private boolean enoughTicketsMrX(MoveDouble move, int numSectetTickets)
+    {
+    	int numDouble = getPlayerTickets(Colour.valueOf("Black"), Ticket.valueOf("DoubleMove"));
+    	if(numDouble <= 0) return false;
+    	String[] substrings = move.toString().split(" -> | |: ");
+    	Colour player = Colour.valueOf(substrings[2]);
+    	int m1Target = Integer.parseInt(substrings[3]);
+    	Ticket m1Ticket = Ticket.valueOf(substrings[4]);
+    	int m2Target = Integer.parseInt(substrings[5]);
+    	Ticket m2Ticket = Ticket.valueOf(substrings[6]);
+    	MoveTicket firstMove = new MoveTicket(player, m1Target, m1Ticket);
+    	MoveTicket secondMove = new MoveTicket(player, m2Target, m2Ticket);
+    	if( !enoughTicketsMrX(firstMove, numSectetTickets) ) return false;
+    	if(m1Ticket.equals(Ticket.valueOf("SecretMove"))) numSectetTickets --;
+    	if( !enoughTicketsMrX(secondMove, numSectetTickets) ) return false;
+    	else return true;
+    }
+    
+    //checks, for a single moves, that for given move MrX has got 
+    //enough ticekts to make move
+    private boolean enoughTicketsMrX(MoveTicket move, int numSectetTickets)
+    {
+    	String[] substrings = move.toString().split(" ");
+    	Ticket ticket = Ticket.valueOf(substrings[2]);
+    	if(ticket.equals(Ticket.valueOf("SecretMove")))
+    	{
+    		if(numSectetTickets > 0) return true;
+    		else return false;
+    	}
+    	return true;
+    }
+    
+    //checks, for a single moves, that for given move a detective has got 
+    //enough ticekts to make move
+    private boolean enoghTicketsDetective(MoveTicket move, int numBus, int numTaxi, int numUnderground)
+    {
+    	String[] substrings = move.toString().split(" ");
+    	Ticket ticket = Ticket.valueOf(substrings[2]);
+    	if(ticket.equals(Ticket.valueOf("Bus")) && numBus > 0) return true;
+		else if(ticket.equals(Ticket.valueOf("Taxi")) && numTaxi > 0) return true;
+		else if(ticket.equals(Ticket.valueOf("Underground")) && numUnderground > 0) return true;
+		else return false;
+    }
+    
+    //checks if player (bar player making @move) is at either 2 nodes making 
+    //up double move
+    private boolean moveOcupyTest(MoveDouble move)
+    {
+    	String[] substrings = move.toString().split(" -> | |: ");
+    	Colour player = Colour.valueOf(substrings[2]);
+    	int m1Target = Integer.parseInt(substrings[3]);
+    	Ticket m1Ticket = Ticket.valueOf(substrings[4]);
+    	int m2Target = Integer.parseInt(substrings[5]);
+    	Ticket m2Ticket = Ticket.valueOf(substrings[6]);
+    	MoveTicket firstMove = new MoveTicket(player, m1Target, m1Ticket);
+    	MoveTicket secondMove = new MoveTicket(player, m2Target, m2Ticket);
+    	if( !moveOcupyTest(firstMove) && !moveOcupyTest(secondMove) )
+    	{
+    		return false;
+    	}
+    	else return true;
+    }
+    
+    //checks if player (bar player making @move) is at target node
+    private boolean moveOcupyTest(MoveTicket move)
+    {
+    	String[] substrings = move.toString().split(" ");
+    	int target = Integer.parseInt(substrings[substrings.length - 2]);
+    	String color =  substrings[0];
+    	int currentLocation = colourToLocation.get(Colour.valueOf(color));
+    	if(currentLocation == target || !ocupy(target) ) return false;
+    	else return true;
+    }
+    
+    //returns true if a player is currently at the given node 
+    private boolean ocupy(int node)
+    {
+    	for(Colour player : orderOfPlay)
+    	{
+    		if(colourToLocation.get(player).equals(node)) return true;
+    	}
+    	return false;
+    }
+    
+    // returns all posible moves (only double if player is MrX but this is 
+    //only valodation done) for given player at given location
+    private List<MoveDouble> doubleMoves(Colour player)
+    {
+    	int location = colourToLocation.get(player);
+    	List<MoveDouble> possibleMoves = new ArrayList();
+    	List<MoveTicket> singleMoves = singleMoves(location, player);
+    	if(player == Colour.valueOf("Black"))
+    	{
+    		for(MoveTicket fisrtMove : singleMoves)
+    		{
+    			String[] substrings = fisrtMove.toString().split(" ");
+    			int target = Integer.parseInt(substrings[substrings.length - 2]);
+    			for(MoveTicket secondMove : singleMoves(target, player))
+    			{
+    				possibleMoves.add(new MoveDouble(player, fisrtMove, secondMove));
+    			}
+    		}
+    	}
+    	return possibleMoves;
+    }
+    
+    //returns list of unValidated moveTickets for @player at @location
+    private List<MoveTicket> singleMoves(int location, Colour player)
+    {
+    	List<MoveTicket> possibleMoveTickets = new ArrayList();
+    	List<Edge<Integer, Route>> possibleEdges  = londonGraph.getEdges(location);
+    	for (Edge<Integer, Route> edge : possibleEdges)
+    	{
+    		possibleMoveTickets.add(edgeToTicket(location, player, edge));
+    	}
+    	return possibleMoveTickets;
+    }
+    
+    //produces move for @player at @location moving along @edge
+    private MoveTicket edgeToTicket(int location, Colour player, Edge<Integer, Route> edge)
+    {
+    	int target = edge.other(location);
+        Ticket moveType =  Ticket.fromRoute(edge.data());
+        return new MoveTicket(player, target, moveType);
     }
 
     @Override
@@ -128,7 +299,7 @@ private final List<Boolean> showRounds;
 
     @Override
     public Colour getCurrentPlayer() {
-        return null;
+        return currentPlayer;
     }
 
     @Override
